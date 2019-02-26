22693378e741d075c623912985972aefad868fec
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 20:39:01 +0700

    commit

--- php/add-mask.php
+++ php/add-mask.php
@@ -7,5 +7,6 @@ $desc = $_GET["desc"];
 $link = $_GET["link"];
 $imgURL = $_GET["img_url"];
 $maskId = uniqid();
-$c->query("INSERT INTO masks (id, name, img_url, link, mask_code, filter_code, descr, price) VALUES ('" . $maskId . "', '" . $name . "', '" . $imgURL . "', '" . $link . "', '" . $desc . "', " . $price . ")");
-$c->query("INSERT INTO rewards (id, mask_id, points, mask_code, filter_code, name, img_url, descr, url) VALUES ('" . uniqid() . "', '" . $maskId . "', " . $points . ", '" . $name . "', '" . $imgURL . "', '" . $desc . "', '" . $link . "')");
+//$c->query("INSERT INTO masks (id, name, img_url, link, mask_code, filter_code, descr, price) VALUES ('" . $maskId . "', '" . $name . "', '" . $imgURL . "', '" . $link . "', '" . $desc . "', " . $price . ")");
+$c->query("INSERT INTO masks (id, name, img_url, link, mask_code, filter_code, descr, price) VALUES ('5', 'Mask2', 'google.com', 'yahoo.com', '123abc', '456abc', 'Hello2', 30000);");
+//$c->query("INSERT INTO rewards (id, mask_id, points, mask_code, filter_code, name, img_url, descr, url) VALUES ('" . uniqid() . "', '" . $maskId . "', " . $points . ", '" . $name . "', '" . $imgURL . "', '" . $desc . "', '" . $link . "')");
