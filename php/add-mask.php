7acbe964637a83398bc5a7548b52d9c3aff3da6d
Dana Prakoso <danaossoftware@gmail.com>
2019-02-26 20:36:45 +0700

    commit

--- php/add-mask.php
+++ php/add-mask.php
@@ -1,11 +1,11 @@
 <?php
 include 'db.php';
-$name = $_POST["name"];
-$price = intval($_POST["price"]);
-$points = intval($_POST["points"]);
-$desc = $_POST["desc"];
-$link = $_POST["link"];
-$imgURL = $_POST["img_url"];
+$name = $_GET["name"];
+$price = intval($_GET["price"]);
+$points = intval($_GET["points"]);
+$desc = $_GET["desc"];
+$link = $_GET["link"];
+$imgURL = $_GET["img_url"];
 $maskId = uniqid();
 $c->query("INSERT INTO masks (id, name, img_url, link, mask_code, filter_code, descr, price) VALUES ('" . $maskId . "', '" . $name . "', '" . $imgURL . "', '" . $link . "', '" . $desc . "', " . $price . ")");
 $c->query("INSERT INTO rewards (id, mask_id, points, mask_code, filter_code, name, img_url, descr, url) VALUES ('" . uniqid() . "', '" . $maskId . "', " . $points . ", '" . $name . "', '" . $imgURL . "', '" . $desc . "', '" . $link . "')");
